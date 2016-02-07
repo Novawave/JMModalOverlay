300de9eb456c6ff977fd2ac9090a1431c5caa055
mz2 <matias.piipari@gmail.com>
2016-02-07 18:43:37 +0000

    Adds an umbrella header.

---
+++ JMModalOverlay/JMModalOverlay-Framework.h
@@ -0,0 +1,9 @@
+//
+//  JMModalOverlay-Framework.h
+//  JMModalOverlay
+//
+//  Created by Matias Piipari on 07/02/2016.
+//  Copyright © 2016 Jérémy Marchand. All rights reserved.
+//
+
+#import "JMModalOverlay.h"
